95645226efd93aba2b6866c0f8d4456b6197eed1
oamairi <oamairi@f1br10s7.paris.42.school>
2026-03-04 14:32:37 +0100

    v1.3

--- ex03/Weapon.hpp
+++ ex03/Weapon.hpp
@@ -6,7 +6,7 @@
 /*   By: oamairi <oamairi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/03 15:00:32 by oamairi           #+#    #+#             */
-/*   Updated: 2026/03/04 14:18:40 by oamairi          ###   ########.fr       */
+/*   Updated: 2026/03/04 14:27:17 by oamairi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ class Weapon
 {
 private:
 	std::string type;
-	public:
+public:
 	Weapon();
 	Weapon(std::string name);
 	void	setType(std::string type);
